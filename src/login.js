a2b887ef11fbae0832483b3423b0bf64cbbf3377
Zhen Yu Yong <fuz3zy@gmail.com>
2020-05-13 12:10:18 +0800

    added verifer id into verifier params

--- src/login.js
+++ src/login.js
@@ -212,6 +212,7 @@ class DirectWebSDK {
     console.log("token seeds", aggregateIdTokenSeeds);
     log.info("token seeds", aggregateIdTokenSeeds);
     const aggregateIdToken = Web3Utils.keccak256(aggregateIdTokenSeeds.join(String.fromCharCode(29))).slice(2);
+    aggregateVerifierParams.verifier_id = aggregateVerifierId;
     const pubKeyDetails = this.handleSingleLogin(verifier, aggregateVerifierId, aggregateVerifierParams, aggregateIdToken);
     return {
       verifierId: aggregateVerifierId,
