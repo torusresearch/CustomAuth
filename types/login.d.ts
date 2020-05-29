5f9b72eaa8c0c89b6db5e409b600475790d8c97e
chaitanyapotti <chaitanya.potti@gmail.com>
2020-05-29 19:04:02 +0800

    Update typings

--- types/login.d.ts
+++ types/login.d.ts
@@ -10,7 +10,7 @@ declare class DirectWebSDK {
 
   init(): Promise<void>;
 
-  triggerLogin({ typeOfLogin, verifier, clientId }: SubVerifierDetails): Promise<TorusLoginResponse>;
+  triggerLogin({ typeOfLogin, verifier, clientId, jwtParams }: SubVerifierDetails): Promise<TorusLoginResponse>;
 
   triggerAggregateLogin(
     aggregateVerifierType: AGGREGATE_VERIFIER_TYPE,
@@ -78,6 +78,7 @@ interface SubVerifierDetails {
   typeOfLogin: LOGIN_TYPE;
   verifier: string;
   clientId: string;
+  jwtParams?: Auth0ClientOptions;
 }
 
 declare enum ETHEREUM_NETWORK {
@@ -101,3 +102,139 @@ declare enum AGGREGATE_VERIFIER_TYPE {
   // AND_AGGREGATE_VERIFIER = "and_aggregate_verifier",
   // OR_AGGREGATE_VERIFIER = "or_aggregate_verifier",
 }
+
+// REGION: AUTH0 PARAMS
+interface BaseLoginOptions {
+  /**
+   * - `'page'`: displays the UI with a full page view
+   * - `'popup'`: displays the UI with a popup window
+   * - `'touch'`: displays the UI in a way that leverages a touch interface
+   * - `'wap'`: displays the UI with a "feature phone" type interface
+   */
+  display?: "page" | "popup" | "touch" | "wap";
+  /**
+   * - `'none'`: do not prompt user for login or consent on reauthentication
+   * - `'login'`: prompt user for reauthentication
+   * - `'consent'`: prompt user for consent before processing request
+   * - `'select_account'`: prompt user to select an account
+   */
+  prompt?: "none" | "login" | "consent" | "select_account";
+  /**
+   * Maximum allowable elasped time (in seconds) since authentication.
+   * If the last time the user authenticated is greater than this value,
+   * the user must be reauthenticated.
+   */
+  max_age?: string | number;
+  /**
+   * The space-separated list of language tags, ordered by preference.
+   * For example: `'fr-CA fr en'`.
+   */
+  ui_locales?: string;
+  /**
+   * Previously issued ID Token.
+   */
+  id_token_hint?: string;
+  /**
+   * The user's email address or other identifier. When your app knows
+   * which user is trying to authenticate, you can provide this parameter
+   * to pre-fill the email box or select the right session for sign-in.
+   *
+   * This currently only affects the classic Lock experience.
+   */
+  login_hint?: string;
+  acr_values?: string;
+  /**
+   * The default scope to be used on authentication requests.
+   * The defaultScope defined in the Auth0Client is included
+   * along with this scope
+   */
+  scope?: string;
+  /**
+   * The default audience to be used for requesting API access.
+   */
+  audience?: string;
+  /**
+   * The name of the connection configured for your application.
+   * If null, it will redirect to the Auth0 Login Page and show
+   * the Login Widget.
+   */
+  connection?: string;
+
+  /**
+   * If you need to send custom parameters to the Authorization Server,
+   * make sure to use the original parameter name.
+   */
+  [key: string]: unknown;
+}
+
+interface AdvancedOptions {
+  /**
+   * The default scope to be included with all requests.
+   * If not provided, 'openid profile email' is used. This can be set to `null` in order to effectively remove the default scopes.
+   *
+   * Note: The `openid` scope is **always applied** regardless of this setting.
+   */
+  defaultScope?: string;
+}
+
+interface Auth0ClientOptions extends BaseLoginOptions {
+  /**
+   * Your Auth0 account domain such as `'example.auth0.com'`,
+   * `'example.eu.auth0.com'` or , `'example.mycompany.com'`
+   * (when using [custom domains](https://auth0.com/docs/custom-domains))
+   */
+  domain: string;
+  /**
+   * The issuer to be used for validation of JWTs, optionally defaults to the domain above
+   */
+  issuer?: string;
+  /**
+   * The Client ID found on your Application settings page
+   */
+  client_id: string;
+  /**
+   * The default URL where Auth0 will redirect your browser to with
+   * the authentication result. It must be whitelisted in
+   * the "Allowed Callback URLs" field in your Auth0 Application's
+   * settings. If not provided here, it should be provided in the other
+   * methods that provide authentication.
+   */
+  redirect_uri?: string;
+  /**
+   * The value in seconds used to account for clock skew in JWT expirations.
+   * Typically, this value is no more than a minute or two at maximum.
+   * Defaults to 60s.
+   */
+  leeway?: number;
+
+  /**
+   * The location to use when storing cache data. Valid values are `memory` or `localstorage`.
+   * The default setting is `memory`.
+   */
+  cacheLocation?: CacheLocation;
+
+  /**
+   * If true, refresh tokens are used to fetch new access tokens from the Auth0 server.
+   * If false, the legacy technique of using a hidden iframe and the `authorization_code` grant with `prompt=none` is used.
+   * The default setting is `false`.
+   *
+   * **Note**: Use of refresh tokens must be enabled by an administrator on your Auth0 client application.
+   */
+  useRefreshTokens?: boolean;
+
+  /**
+   * A maximum number of seconds to wait before declaring background calls to /authorize as failed for timeout
+   * Defaults to 60s.
+   */
+  authorizeTimeoutInSeconds?: number;
+
+  /**
+   * Changes to recommended defaults, like defaultScope
+   */
+  advancedOptions?: AdvancedOptions;
+}
+
+/**
+ * The possible locations where tokens can be stored
+ */
+declare type CacheLocation = "memory" | "localstorage";
