93727e678b58ce78333ff08f532e27843bcbbe10
chaitanyapotti <chaitanya.potti@gmail.com>
2020-06-10 11:16:23 +0800

    use location.origin

--- examples/vue-app/src/App.vue
+++ examples/vue-app/src/App.vue
@@ -84,7 +84,7 @@ export default {
     async login() {
       try {
         const torusdirectsdk = new TorusSdk({
-          baseUrl: "http://localhost:3000/serviceworker",
+          baseUrl: `${location.origin}/serviceworker`,
           enableLogging: true,
           proxyContractAddress: "0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183", // details for test net
           network: "ropsten", // details for test net
