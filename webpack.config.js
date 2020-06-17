6c42f727f5c91013c69aaf8fd17ae83c08511c03
chaitanyapotti <chaitanya.potti@gmail.com>
2020-06-17 13:19:21 +0800

    - bump packages - resolve alias to reduce build size

--- webpack.config.js
+++ webpack.config.js
@@ -18,11 +18,16 @@ const baseConfig = {
     library: libraryName,
     libraryExport: "default",
   },
-  module: {
-    rules: [],
-  },
   resolve: {
     extensions: [".ts", ".js", ".json"],
+    alias: {
+      "bn.js": path.resolve(__dirname, "node_modules/bn.js"),
+      lodash: path.resolve(__dirname, "node_modules/lodash"),
+      "js-sha3": path.resolve(__dirname, "node_modules/js-sha3"),
+    },
+  },
+  module: {
+    rules: [],
   },
 };
 
