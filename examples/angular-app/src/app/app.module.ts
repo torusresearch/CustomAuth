ee26dd2346b2b71c72aaa410a385ee14d5833589
Lionell Briones <llenoil@gmail.com>
2020-08-05 09:14:51 +0800

    Organinze angular example

--- examples/angular-app/src/app/app.module.ts
+++ examples/angular-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
