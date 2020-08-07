523bcbeb6c8edeae40bf0b8c93c158ef876b9a10
chaitanyapotti <chaitanya.potti@gmail.com>
2020-08-07 10:55:27 +0800

    use testnet instead of ropsten

--- examples/react-app/src/App.js
+++ examples/react-app/src/App.js
@@ -75,7 +75,7 @@ class App extends React.Component {
         baseUrl: `${window.location.origin}/serviceworker`,
         enableLogging: true,
         proxyContractAddress: "0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183", // details for test net
-        network: "ropsten", // details for test net
+        network: "testnet", // details for test net
       });
 
       await torusdirectsdk.init({ skipSw: false });
