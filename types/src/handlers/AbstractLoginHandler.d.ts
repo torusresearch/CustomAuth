4cb84f5423871cf9851269aa0510db01231b7a94
chaitanyapotti <chaitanya.potti@gmail.com>
2020-09-16 11:22:55 +0800

    types

--- types/src/handlers/AbstractLoginHandler.d.ts
+++ types/src/handlers/AbstractLoginHandler.d.ts
@@ -1,14 +1,15 @@
 import { LOGIN_TYPE } from "../utils/enums";
-import { ILoginHandler, LoginWindowResponse, TorusVerifierResponse } from "./interfaces";
+import { Auth0ClientOptions, ILoginHandler, LoginWindowResponse, TorusVerifierResponse } from "./interfaces";
 export default abstract class AbstractLoginHandler implements ILoginHandler {
     readonly clientId: string;
     readonly verifier: string;
     readonly redirect_uri: string;
     readonly typeOfLogin: LOGIN_TYPE;
     readonly redirectToOpener?: boolean;
+    readonly jwtParams?: Auth0ClientOptions;
     protected nonce: string;
     protected finalURL: URL;
-    constructor(clientId: string, verifier: string, redirect_uri: string, typeOfLogin: LOGIN_TYPE, redirectToOpener?: boolean);
+    constructor(clientId: string, verifier: string, redirect_uri: string, typeOfLogin: LOGIN_TYPE, redirectToOpener?: boolean, jwtParams?: Auth0ClientOptions);
     get state(): string;
     abstract getUserInfo(params: LoginWindowResponse): Promise<TorusVerifierResponse>;
     abstract setFinalUrl(): void;
--- types/src/handlers/TwitchHandler.d.ts
+++ types/src/handlers/TwitchHandler.d.ts
@@ -1,15 +1,16 @@
 import { LOGIN_TYPE } from "../utils/enums";
 import AbstractLoginHandler from "./AbstractLoginHandler";
-import { LoginWindowResponse, TorusVerifierResponse } from "./interfaces";
+import { Auth0ClientOptions, LoginWindowResponse, TorusVerifierResponse } from "./interfaces";
 export default class TwitchHandler extends AbstractLoginHandler {
     readonly clientId: string;
     readonly verifier: string;
     readonly redirect_uri: string;
     readonly typeOfLogin: LOGIN_TYPE;
     readonly redirectToOpener?: boolean;
+    readonly jwtParams?: Auth0ClientOptions;
     private readonly RESPONSE_TYPE;
     private readonly SCOPE;
-    constructor(clientId: string, verifier: string, redirect_uri: string, typeOfLogin: LOGIN_TYPE, redirectToOpener?: boolean);
+    constructor(clientId: string, verifier: string, redirect_uri: string, typeOfLogin: LOGIN_TYPE, redirectToOpener?: boolean, jwtParams?: Auth0ClientOptions);
     setFinalUrl(): void;
     getUserInfo(params: LoginWindowResponse): Promise<TorusVerifierResponse>;
 }
