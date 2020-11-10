757b8bb908231a394ebdea16cc00a21f94e842be
chaitanyapotti <chaitanya.potti@gmail.com>
2020-11-10 17:35:31 +0800

    change message

--- examples/vue-app/src/App.vue
+++ examples/vue-app/src/App.vue
@@ -14,10 +14,11 @@
     </div>
     <p>Please note that the verifiers listed in the example have http://localhost:3000/serviceworker/redirect configured as the redirect uri.</p>
     <p>If you use any other domains, they won't work.</p>
+    <p>The verifiers listed here only work with the client id's specified in example. Please don't edit them</p>
     <p>The verifiers listed here are for example reference only. Please don't use them for anything other than testing purposes.</p>
     <div>
       Reach out to us at <a href="mailto:hello@tor.us">hello@tor.us</a> or <a href="https://t.me/torusdev">telegram group</a> to get your verifier
-      deployed.
+      deployed for your client id.
     </div>
     <div id="console">
       <p></p>
