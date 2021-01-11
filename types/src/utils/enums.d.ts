665c160f5f1d474da2d34064e78a29db51b1e4d4
chaitanyapotti <chaitanya.potti@gmail.com>
2021-01-11 11:20:48 +0800

    types

--- types/src/utils/enums.d.ts
+++ types/src/utils/enums.d.ts
@@ -28,3 +28,7 @@ export declare const AGGREGATE_VERIFIER: {
 export declare type TORUS_NETWORK_TYPE = typeof TORUS_NETWORK[keyof typeof TORUS_NETWORK];
 export declare type LOGIN_TYPE = typeof LOGIN[keyof typeof LOGIN];
 export declare type AGGREGATE_VERIFIER_TYPE = typeof AGGREGATE_VERIFIER[keyof typeof AGGREGATE_VERIFIER];
+export declare const CONTRACT_MAP: {
+    mainnet: string;
+    testnet: string;
+};
