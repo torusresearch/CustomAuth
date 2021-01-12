8a0db060e6443e7cd1cc0dc6bff6c1f1cceb55f5
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-12 12:31:37 +0700

    Add option to opt out

--- src/utils/loglevel.ts
+++ src/utils/loglevel.ts
@@ -1,31 +1,36 @@
 import { BrowserClient, Hub } from "@sentry/browser";
 import loglevel from "loglevel";
 
-const dsn = process.env.SENTRY_DSN;
-if (!dsn) {
-  throw new Error("Sentry DSN is not set.");
-}
-
-const sentry = new Hub(
-  new BrowserClient({
-    dsn,
-    sampleRate: 1,
-  })
-);
-
 const log = loglevel.getLogger("torus-direct-web-sdk");
 
-// Hook Sentry into log.error.
-const originalFactory = log.methodFactory;
-log.methodFactory = (method, level, name) => {
-  if (method === "error") {
-    return (err) => {
-      sentry.captureException(err);
-    };
-  }
-  return originalFactory(method, level, name);
-};
-
-log.setLevel(log.getLevel()); // Apply changes.
-
 export default log;
+
+let sentry: Hub;
+
+export const enableErrorTracking = (): void => {
+  if (sentry) return; // Already setup.
+
+  // Setup Sentry.
+  const dsn = process.env.SENTRY_DSN;
+  if (!dsn) throw new Error("Sentry DSN is not set.");
+  sentry = new Hub(
+    new BrowserClient({
+      dsn,
+      sampleRate: 1,
+    })
+  );
+
+  // Hook Sentry into log.error.
+  const defaultFactory = log.methodFactory;
+  log.methodFactory = (method, level, name) => {
+    if (method === "error") {
+      return (err) => {
+        sentry.captureException(err);
+      };
+    }
+    return defaultFactory(method, level, name);
+  };
+
+  // Apply changes.
+  log.setLevel(log.getLevel());
+};
