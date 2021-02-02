6a03174afe12392ecf28cbab7963c615a2fa224f
chaitanyapotti <chaitanya.potti@gmail.com>
2021-02-02 18:01:10 +0800

    Export createHandler type

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import DirectWebSdk from "./src/login";
-
-export default DirectWebSdk;
+export { default } from "./src/login";
 export * from "./src/handlers/interfaces";
 export * from "./src/utils/enums";
+export { default as createHandler } from "./src/handlers/HandlerFactory";
