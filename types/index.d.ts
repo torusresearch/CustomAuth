70ae8a5272feed3688b1eef6b29effc14e47104b
chaitanyapotti <chaitanya.potti@gmail.com>
2021-02-02 18:25:31 +0800

    - Remove default library export - Update deps - Lint

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-export { default } from "./src/login";
-export * from "./src/handlers/interfaces";
-export * from "./src/utils/enums";
 export { default as createHandler } from "./src/handlers/HandlerFactory";
+export * from "./src/handlers/interfaces";
+export { default } from "./src/login";
+export * from "./src/utils/enums";
