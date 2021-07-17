e62567a21be6679072fa781dcc45292f1decfb5d
himanshuchawla009 <himanshuchawla2014@gmail.com>
2021-07-17 21:08:40 +0530

    redirect flow added in react example

--- examples/react-app/src/index.tsx
+++ examples/react-app/src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+import RouterPage from './router';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+
+      <RouterPage />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
