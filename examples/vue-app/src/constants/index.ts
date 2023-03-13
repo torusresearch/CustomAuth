dbc813ecc6b091000da7372fc654c56cfa7d4acb
Leonard Tan <lentan1029@gmail.com>
2023-03-14 03:38:00 +0800

    modify example to use passkeys

--- examples/vue-app/src/constants/index.ts
+++ examples/vue-app/src/constants/index.ts
@@ -13,7 +13,8 @@ export const EMAIL_PASSWORD = "email_password";
 export const PASSWORDLESS = "passwordless";
 export const HOSTED_EMAIL_PASSWORDLESS = "hosted_email_passwordless";
 export const HOSTED_SMS_PASSWORDLESS = "hosted_sms_passwordless";
-export const WEBAUTHN = "webauthn";
+export const WEBAUTHN_LOGIN = "webauthn_login";
+export const WEBAUTHN_REGISTER = "webauthn_register";
 export const COGNITO = "cognito";
 export const AUTH_DOMAIN = "https://torus-test.auth0.com";
 export const COGNITO_AUTH_DOMAIN = "https://torus-test.auth.ap-southeast-1.amazoncognito.com/oauth2/";
@@ -55,7 +56,8 @@ export const verifierMap = {
     clientId: "nSYBFalV2b1MSg5b2raWqHl63tfH3KQa",
     verifier: "torus-auth0-sms-passwordless",
   },
-  [WEBAUTHN]: { name: "WebAuthn", typeOfLogin: "webauthn", clientId: "webauthn", verifier: "webauthn-lrc" },
+  [WEBAUTHN_LOGIN]: { name: "WebAuthn Login", typeOfLogin: "webauthn", clientId: "webauthn", verifier: "webauthntest" },
+  [WEBAUTHN_REGISTER]: { name: "WebAuthn Register", typeOfLogin: "webauthn", clientId: "webauthn", verifier: "webauthntest" },
   [COGNITO]: {
     name: "Cognito",
     typeOfLogin: "jwt",
--- examples/vue-app/src/views/RedirectMode/Login.vue
+++ examples/vue-app/src/views/RedirectMode/Login.vue
@@ -97,7 +97,7 @@ export default defineComponent({
       redirectPathName: "auth",
       enableLogging: true,
       uxMode: UX_MODE.REDIRECT,
-      network: "testnet",
+      network: "mainnet",
     });
     this.torusdirectsdk = torusdirectsdk;
     await torusdirectsdk.init({ skipSw: true });
@@ -106,13 +106,18 @@ export default defineComponent({
     async login() {
       if (!this.torusdirectsdk) return;
       const jwtParams = this.loginToConnectionMap[this.selectedVerifier] || {};
-      const { typeOfLogin, clientId, verifier } = verifierMap[this.selectedVerifier];
+      const { typeOfLogin, clientId, verifier, name } = verifierMap[this.selectedVerifier];
+      const webauthnRegister = name === "WebAuthn Register"
+      const registerOnly = webauthnRegister ? true : false;
+      const loginOnly = webauthnRegister ? "false" : "true";
+
       return this.torusdirectsdk.triggerLogin({
         typeOfLogin,
         verifier,
         clientId,
         jwtParams,
-        customState: { client: "great-company" },
+        registerOnly,
+        customState: { client: "great-company", webauthnURL: "https://d1f8-115-66-172-125.ngrok.io/", localhostAll: "true", loginOnly, webauthnTransports: 'ble', credTransports: 'ble' },
       });
     },
   },
