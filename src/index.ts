351ed8f02af0815cef8ab259526085bbbc43d004
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-07-15 15:27:45 +0800

    export error serialization

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as createHandler } from "./handlers/HandlerFactory";
 export * from "./handlers/interfaces";
 export { default } from "./login";
 export * from "./utils/enums";
+export * from "./utils/error";
 export * from "./utils/helpers";
