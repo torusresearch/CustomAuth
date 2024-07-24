7a9c0af33a3e100bf64bc65328f9ef9f6ef793eb
hieu-w <hieu@tor.us>
2024-07-25 00:31:30 +0700

    Fix Alias @

--- examples/vue-app/vite.config.js
+++ examples/vue-app/vite.config.js
@@ -1,7 +1,6 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
-import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +13,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
